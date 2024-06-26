4102ab132dcf76ba8b11b8967903b7335934127f
ytraub <y.traub@outlook.com>
2024-06-26 15:05:19 +0200

    Some more testing

--- backend/runs.py
+++ backend/runs.py
@@ -8,4 +8,7 @@ class Test:
 		self.run_name = 'test'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
-		await multitask(robot.drive_forward(distance=1000,speed=20),robot.module_left(distance=1000,speed=50))
+		await robot.drive_forward(distance=200,speed=20)
+		await robot.module_right(distance=1000,speed=20)
+		await multitask(robot.module_left(distance=1000,speed=50),robot.drive_forward(distance=1000,speed=20))
+		await robot.drive_backward(speed=60,distance=300)
