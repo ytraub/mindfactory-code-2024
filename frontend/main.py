0ab547c5d9a8e5caba530ba6af05776fcbfd29da
ytraub <y.traub@outlook.com>
2024-06-27 09:32:23 +0200

    Fixed error

--- frontend/main.py
+++ frontend/main.py
@@ -13,6 +13,7 @@ def main() -> None:
     file_buffer = '"""\nThis is a generated file. Don\'t change anything manually.\n"""\n\nfrom pybricks.tools import multitask\nfrom robot import Robot\n\n'
 
     for filename in os.listdir(RUN_DIRECTORY):
+        print(filename)
         if not filename.endswith(".run"):
             continue
 
@@ -24,14 +25,14 @@ def main() -> None:
                 log_error("Lexing", filename, error)
                 return
 
-            (output, error) = compiler.compile(tokens, filename.strip(".run"))
+            (output, error) = compiler.compile(tokens, filename.removesuffix(".run"))
             if error:
                 log_error("Parsing", filename, error)
                 return
 
             file_buffer += output
 
-    with open("output/runs.py", "w") as output:
+    with open("output/runs.py", "w+") as output:
         output.write(file_buffer)
 
 
--- frontend/output/runs.py
+++ frontend/output/runs.py
@@ -5,23 +5,23 @@ This is a generated file. Don't change anything manually.
 from pybricks.tools import multitask
 from robot import Robot
 
-class D:
+class Run1:
 	def __init__(self):
-		self.run_name = 'd'
+		self.run_name = 'run1'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
 		await robot.drive_backward(speed=60,distance=300)
 
-class S:
+class Run2:
 	def __init__(self):
-		self.run_name = 's'
+		self.run_name = 'run2'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
 		await robot.drive_backward(speed=60,distance=300)
 
-class Test2:
+class Run3:
 	def __init__(self):
-		self.run_name = 'test2'
+		self.run_name = 'run3'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
 		await robot.drive_backward(speed=60,distance=300)
