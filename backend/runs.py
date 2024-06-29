84a5f408f38c015f582e36462885fc8d70a63807
ytraub <y.traub@outlook.com>
2024-06-29 08:25:31 +0200

    Remove print

--- backend/runs.py
+++ backend/runs.py
@@ -19,18 +19,18 @@ class Run2:
 		self.run_name = 'run2'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
-		await robot.drive_forward(speed=100,distance=100)
-		await robot.drive_backward(distance=500,speed=30)
+		await robot.drive_forward(speed=100,distance=400)
 		await multitask(robot.drive_forward(speed=100,distance=300),robot.module_left(speed=60,distance=200))
+		await robot.drive_backward(distance=100,speed=30)
 
 class Run3:
 	def __init__(self):
 		self.run_name = 'run3'
 		self.run_color = 'RED'
 	async def execute(self, robot: Robot):
-		await robot.drive_forward(speed=100,distance=100)
-		await robot.drive_backward(distance=500,speed=30)
-		await multitask(robot.drive_forward(speed=100,distance=300),robot.module_left(speed=60,distance=200))
+		await multitask(robot.drive_forward(speed=100,distance=300),robot.module_left(speed=60,distance=500))
+		await robot.drive_forward(speed=100,distance=140)
+		await robot.drive_backward(distance=240,speed=45)
 
 
 runs = [
